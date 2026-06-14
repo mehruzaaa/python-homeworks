39994b23b6264981211bb0beb6caa46fe120b77d
mehruzaaa <mehruzap@icloud.com>
2026-06-14 19:47:56 +0500

    Some files added

---
+++ string_intro.py
@@ -0,0 +1,53 @@
+# ' - single quote
+# " - double quote
+# \ - backslash
+# / - forward slash
+# \n - newline character
+# \' - single quote
+# \" - double quote
+# \t - tab
+# \\ - backslash
+
+#hello = "Hello \\nW\\norld"
+
+
+#word = "I\"m a teacher"
+# print(word)
+#print(hello)
+
+
+#directory = r"D:\Python\bi-and-ai-talents\lesson-2\homewrok"
+#print(type(directory))
+#print(directory)
+
+#
+my_str = "Hello world" # Sequence
+#print(len(my_str))
+
+#Slicing or Indexing
+# BEGIN:END:STEP
+#print(my_str[:4] + my_str[5:])
+#print(my_str[::2])
+
+
+my_str = "Hello world" # Sequence
+
+#print(my_str[-5:])
+
+#SyntaxError
+#TypeError
+#ValueError
+#IndexError: string index out of range
+
+#Mutable vs Immutable (O'zgaruvchan vs O'zgarmas
+
+my_str = "Apple"
+print("Oldin:", id(my_str))
+
+my_str = 'a' + my_str[1:]
+print("Keyin", id(my_str))
+
+print(my_str, my_str2)
+
+# object.method()
+
---
+++ string_simmple_app.py
@@ -0,0 +1,20 @@
+name = "John" #input("What is your name? ")
+age = 30 # int(input("Age: "))
+
+#int("Your name is " + name + " and you are " + str(age) + " yearld old. ")
+#print(f"Your name is {name} and you are {age} yearld old. ")
+
+#print("Your name is {fname} and you are {Age} yearld old.".format(Age=age, fname=name))
+#print("Your name is {} and you are {} yearld old.".format(name, age))
+
+# + stringlar uchun "contatenation
+
+
+#n = 34.8765654
+#print(f"{n:5f}")
+#print(n)
+
+name1 = "Adam"
+name2 = "Johnatan"
+#print(f"{name1:<8} - first name")
+#print(f"{name2} - second name")
