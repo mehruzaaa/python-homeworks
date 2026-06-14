683d27b938e524a62617eebf6ade88e99e184570
mehruzaaa <mehruzap@icloud.com>
2026-06-15 02:24:42 +0500

    update

---
+++ lesson1homework.py
@@ -0,0 +1,26 @@
+"""Lesson1 homework
+
+Q1:Why do you think companies analyze large volumes of data?
+A1:Because running a business on "gut feelings" is a great way to lose a lot of money fast. Every time a customer buys something, clicks a link, or complains, they're leaving a footprint. When a company collects thousands of these footprints, they start seeing the big picture. It shows them exactly what people actually love, what's wasting money, and what's about to trend next month. It takes the guesswork out of the room so that companies can thrive.
+
+Q2: If analyzing and sorting large data manually in Excel is difficult, how do you think Python can help solve this problem?
+A2:Excel is good for making a quick budget or a simple chart. But if you try to put a million rows of data into it, computer starts sounding like an engine and the whole app crashes. Plus, humans make mistakes, you drag a formula down wrong, or you accidentally delete a row, and suddenly your whole report is ruined.
+
+Python doesn't get tired or overwhelmed. Instead of opening a massive visual spreadsheet, you write a few lines of code that says, "Hey, clean up this mess, sort it by date, and give me the total." You press enter, it happens in seconds, and the best part? You can use that exact same code tomorrow on a brand-new batch of data without doing any manual work again.
+
+Q3: Imagine you work at a sales company that receives data about 10,000 customer transactions daily. How would you analyze this data?
+A3: I would use python to handle the situation. Python grabs the daily sales file and automatically fixes any weird typos, missing email addresses, or duplicate orders.It can instantly calculates how much money we made today and what our average order price was.
+Or checks what was sold the most and when people bought it.
+
+Q4:In your opinion, what tasks can Python be useful for in BI processes? (e.g., data cleaning, visualization, etc.)
+A:Cleaning data, Analysing data, Automating reports, Creating visualizations, Forecasting, Working with databases, Automating repetitive tasks and so on.
+
+Q5: If you wanted to compare a company's profit year by year, how could this be done using Python?
+A:You can compare a company's profit year by year by putting the profit data into a Python list or table and then calculating the change between consecutive years.
+
+Q6: If you don't know Python, what difficulties might you face when working with large datasets?
+A:If you don't know Python, working with large datasets can be more challenging because tasks like cleaning data, organizing information, analyzing trends, and automating repetitive processes often take much more time and effort. You may also find it harder to handle complex datasets efficiently."""
+
+
+
+
